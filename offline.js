3c0b07bdca78c3b5a780b2a0b249d7c5a23f8f9e
marvintech95 <66658788+marvintech95@users.noreply.github.com>
2020-06-09 14:53:48 -0300

    Primeira inserção de arquios no sistema

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1591724600,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"tiledbackground2.png",
+		"tiledbackground.png",
+		"spriteinfo-sheet0.png",
+		"tiledbackground3.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"sprite4-sheet0.png",
+		"sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
